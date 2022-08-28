13b12f1480646b64b277884e3e60b32dbb805203
David-Orangemoon <76855369+David-Orangemoon@users.noreply.github.com>
2022-08-27 20:59:41 -0400

    Create Base.js

---
+++ custom extensions/Base.js
@@ -0,0 +1,38 @@
+class BaseExtension {
+    constructor(runtime) {
+        this.runtime = runtime
+    }
+    
+    getInfo() {
+        return {
+            "id": "Base",
+            "name": "Base",
+            "color1":'#8a45b5',
+            "color2":'#ca91ed',
+            "color3":'#5a0c8a',
+            "blocks": [
+                    {
+                        "opcode": "exampleopcode",
+                        "blockType": "reporter",
+                        "text": "Example Block [b]",
+                        "arguments": {
+                            "b": {
+                                "type": "number",
+                                "defaultValue": "1"
+                            }
+                        },                    
+                    }
+            ],           
+        };
+    }
+    
+    exampleopcode({b,pr,i}) {
+        return "Hello World"
+    }
+}
+
+(function() {
+    var extensionInstance = new ScratchGamepad(window.vm.extensionManager.runtime)
+    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
+    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
+})()
