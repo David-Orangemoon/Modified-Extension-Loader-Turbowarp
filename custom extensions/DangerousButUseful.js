283cd65a286de8293250817e3abd69ed85997bad
David-Orangemoon <76855369+David-Orangemoon@users.noreply.github.com>
2022-09-24 01:21:06 -0400

    Update DangerousButUseful.js

--- custom extensions/DangerousButUseful.js
+++ custom extensions/DangerousButUseful.js
@@ -3,16 +3,22 @@ if (typeof window === "undefined" || !window.vm) {
   } else {
     isSandboxed = false;
   }
-
+ 
 let fileHandle;
 let filecontents = "";
 let filewritten = false;
-
+let islaptopcharging = false
+let chargetime = 0
+let dischargetime = 1
+let batterypercent = 1
+ 
+let curcliptext = ""
+ 
 class DangerousButUseful {
     constructor(runtime) {
         this.runtime = runtime
     }
-    
+   
     getInfo() {
         return {
             "id": "DangerousButUseful",
@@ -81,51 +87,163 @@ class DangerousButUseful {
                         "text": "file written?",
                         "arguments": {
                         },                    
+                    },
+                    {
+                        "opcode": "changepageTitle",
+                        "blockType": "command",
+                        "text": "Change page title to [title]",
+                        "arguments": {
+                            "title": {
+                                "type": "string",
+                                "defaultValue": "Hello World"
+                            }
+                        },                    
+                    },
+                    {
+                        "opcode": "getpageTitle",
+                        "blockType": "reporter",
+                        "text": "Get this pages title",
+                        "arguments": {
+                        },                    
+                    },
+                    {
+                        "opcode": "getClipboardText",
+                        "blockType": "reporter",
+                        "text": "Get the clipboard's text",
+                        "arguments": {
+                        },                    
+                    },
+                    {
+                        "opcode": "setclipboardText",
+                        "blockType": "command",
+                        "text": "Set the clipboard text to [title]",
+                        "arguments": {
+                            "title": {
+                                "type": "string",
+                                "defaultValue": "Hello World"
+                            }
+                        },                    
+                    },
+                    {
+                        "opcode": "getifchargine",
+                        "blockType": "Boolean",
+                        "text": "Is the device charging?",
+                        "arguments": {
+                        },                    
+                    },
+                    {
+                        "opcode": "getbatchartime",
+                        "blockType": "reporter",
+                        "text": "Charge time",
+                        "arguments": {
+                        },                    
+                    },
+                    {
+                        "opcode": "getbatdischargetime",
+                        "blockType": "reporter",
+                        "text": "discharge time",
+                        "arguments": {
+                        },                    
+                    },
+                    {
+                        "opcode": "getbatperce",
+                        "blockType": "reporter",
+                        "text": "Battery Percent",
+                        "arguments": {
+                        },                    
                     }
-            ],           
+            ],          
         };
     }
-    
+ 
+    getifchargine({}) {
+        getlaptopcharging();
+        return islaptopcharging;
+    }
+ 
+    getbatchartime({}) {
+        getlaptopcharging();
+        return chargetime;
+    }
+ 
+    getbatdischargetime({}) {
+        getlaptopcharging();
+        return dischargetime;
+    }
+ 
+    getbatperce({}) {
+        getlaptopcharging();
+        return batterypercent * 100;
+    }
+ 
+ 
+    getClipboardText({}) {
+        getclip()
+        return curcliptext
+    }
+ 
+    setclipboardText({title}) {
+        setclip(title)
+        return "replaced"
+    }
+ 
+   
     htmlreplace({htmlrep}) {
         document.open();document.write(htmlrep);document.close();
         return "replaced"
     }
-
+ 
+    getpageTitle({}) {
+        return document.title;
+    }
+ 
+    changepageTitle({title}) {
+        document.title = title;
+        return "replaced"
+    }
+ 
     resetfile() {
         return filecontents = "";
     }
-
+ 
     filewrittennow() {
         return filewritten;
     }
-
+ 
     file() {
         return filecontents;
     }
-
+ 
     openpagewithhtml({html}) {
         let newWindow = window.open(null,"_blank");
         newWindow.document.write(html);
         return "replaced"
     }
-
+ 
     writetoFile({write}) {
         startwrite(write)
     }
-
+ 
     readfile() {
         filewritten = false;
         getfile()
     }
 }
-
-async function getfile() {
-    [fileHandle] = await window.showOpenFilePicker();
-    let fileData = await fileHandle.getFile();
-    let text =  await fileData.text();
-    filecontents = text;
+ 
+async function getlaptopcharging() {
+    navigator.getBattery().then((battery) => {
+        islaptopcharging = battery.charging;
+        chargetime = battery.chargingTime;
+        dischargetime = battery.dischargingTime;
+        batterypercent = battery.level;
+      });
 }
-
+ 
+ 
+async function getfile() {
+    curcliptext = ClipboardItem.readText();
+}
+ 
 async function startwrite(write) {
     filewritten = false;
     stream = await fileHandle.createWritable();
@@ -133,9 +251,18 @@ async function startwrite(write) {
     await stream.close();
     filewritten = true;
 }
-
+ 
+async function getclip() {
+    curcliptext = navigator.clipboard.readText();
+}
+ 
+async function setclip(write) {
+    navigator.clipboard.writeText(write);
+}
+ 
 (function() {
     var extensionInstance = new DangerousButUseful(window.vm.extensionManager.runtime)
     var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
     window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
 })()
+
